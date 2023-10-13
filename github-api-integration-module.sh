beea001d636db57f3c15ad11ace717839bc5bccb
Amit Sharma <147665677+amitsharma-2003@users.noreply.github.com>
2023-10-14 03:56:53 +0530

    github-api-integration-module.sh

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -22,7 +22,7 @@ GITHUB_API_REST=$2
 GITHUB_API_HEADER_ACCEPT="Accept: application/vnd.github.v3+json"
 
 temp=`basename $0`
-TMPFILE=`mktemp /tmp/${temp}.XXXXXX` || exit 1
+TMPFILE=`mktemp /tmp/${temp}.XXXXXX` || exit 
 
 
 function rest_call {
